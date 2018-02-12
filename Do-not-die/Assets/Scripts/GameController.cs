f08300276e915a6bc4c7bd4ef5c493527817fc3a
Darren Landig <zyrielsleepwalker@gmail.com>
2018-02-12 14:08:00 +0100

    GameController Start

---
+++ Do-not-die/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public int difficulty = 0;
+    public int life = 3;
+
+
+    //When loading
+    private void Awake() {
+        DontDestroyOnLoad(this);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
