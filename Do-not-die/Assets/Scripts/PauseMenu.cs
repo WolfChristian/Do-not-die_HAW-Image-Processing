ea68800923e4db7d3f901b8cc8d1b6b0bfeb2734
rexyhn <rexyhn3@googlemail.com>
2018-02-13 14:30:27 +0100

    UI

---
+++ Do-not-die/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        
+    }
+    void Quit() {
+        SceneManager.LoadScene(0);
+    }
+}
