ea0934b3013ae04b007458d3e6b7f1b24675e6cf
WolfEngeland <wolfengeland@hotmail.com>
2018-02-13 14:58:51 +0100

    Jump Game first try

---
+++ Do-not-die/Assets/Scripts/JumpGame/GameControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public GameObject gameOverText;
+    public bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ManDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
