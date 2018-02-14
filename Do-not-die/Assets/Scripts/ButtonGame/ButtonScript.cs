72b47d893b4e939260d7b9b0498d492c8cbd6d8f
rexyhn <rexyhn3@googlemail.com>
2018-02-14 16:39:17 +0100

    Fixed Buttons again..

---
+++ Do-not-die/Assets/Scripts/ButtonGame/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+    public GameObject Main;
+    // Use this for initialization
+    void Start () {
+        Main = GameObject.Find("MainController");
+    }
+
+    //Wins game 
+    public void winGame()
+    {
+        Main.GetComponent<MainController>().LoadScene();
+    }
+    //Loses Game
+    public void loseGame()
+    {
+
+        Main.GetComponent<MainController>().loselife();
+
+    }
+}
