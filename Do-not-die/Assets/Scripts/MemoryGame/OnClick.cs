166ae6259517a3c35abeb0edffa25155fe39ec5b
Darren Landig <zyrielsleepwalker@gmail.com>
2018-02-17 15:27:00 +0100

    MemoryGame Beginning

---
+++ Do-not-die/Assets/Scripts/MemoryGame/OnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("CLICKED");
+        this.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    
+}
