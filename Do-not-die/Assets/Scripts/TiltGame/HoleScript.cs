03e615f420dd6f2ef85b7845efdb1cff1b03a499
rexyhn <rexyhn3@googlemail.com>
2018-02-17 16:34:23 +0100

    TiltGame still WIP

---
+++ Do-not-die/Assets/Scripts/TiltGame/HoleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleScript : MonoBehaviour {
+    public GameObject Main;
+    private void Start()
+    {
+        Main = GameObject.Find("MainController");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        loseGame();
+    }
+    //Loses Game
+    public void loseGame()
+    {
+
+        Main.GetComponent<MainController>().loselife();
+
+    }
+}
