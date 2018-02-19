656998e97136b73f173db910685076b1093d370f
rexyhn <rexyhn3@googlemail.com>
2018-02-19 15:15:02 +0100

    Revert "Revert "TiltGame still WIP""
    This reverts commit 96d2d695c7c3d85bd8cc93ad1da36f813eef0b2d.

---
+++ Do-not-die/Assets/Scripts/TiltGame/TiltController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltController : MonoBehaviour {
+    public GameObject Main;
+    float Timer = 100;
+    int diff;
+    // Use this for initialization
+    void Start () {
+        Main = GameObject.Find("MainController");
+
+        //diff = Main.GetComponent<MainController>().difficulty;
+        diff = 1;
+        if (diff < 5) {
+            Timer = 5f;
+        }
+        else if (diff > 5 && diff < 10) {
+            Timer = 4f;
+        }
+        else {
+            Timer = 3f;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void fixUpdate () {
+		
+	}
+     //Wins game 
+  
+    public void loseGame() {
+       
+        Main.GetComponent<MainController>().loselife();
+
+    }
+}
