d56c439dddaef1a61a14727c56d02c09617c4f9b
rexyhn <rexyhn3@googlemail.com>
2018-02-21 18:06:44 +0100

    Trash now Spawns Random

--- Do-not-die/Assets/Scripts/DragGame/DragController.cs
+++ Do-not-die/Assets/Scripts/DragGame/DragController.cs
@@ -7,17 +7,39 @@ public class DragController : MonoBehaviour
 {
     public GameObject Main;
     public GameObject Slider;
+    public GameObject item1, item2, item3, item4;
     float Timer = 100;
     int diff;
     public int food, recycle=0;
+    private  Random rng = new Random();
+    public List<Vector2> positions;
+    public List<GameObject> items;
     // Use this for initialization
     void Start()
     {
         Main = GameObject.Find("MainController");
         Slider = GameObject.Find("TimeBar");
         diff = Main.GetComponent<MainController>().difficulty;
+        positions.Add(new Vector2(-3,0));
+        positions.Add(new Vector2(3, 0));
+        positions.Add(new Vector2(1, 0));
+        positions.Add(new Vector2(-1, 0));
+        Shuffle(positions);
+
+        items.Add(item1);
+        items.Add(item2);
+        items.Add(item3);
+        items.Add(item4);
+
+        for (int i = 0; i < positions.Count; i++) {
+            Instantiate(items[i], positions[i], Quaternion.identity);
+        }
+
+
+
         if (diff < 5) {
             Timer = 8f;
+
         }
         else
         {
@@ -46,5 +68,17 @@ public class DragController : MonoBehaviour
 
         Main.GetComponent<MainController>().loselife();
 
+    }
+    public void spawnObject() {
+        Instantiate(item1, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+    public void Shuffle(List<Vector2> alpha) {
+        for (int i = 0; i < alpha.Count; i++)
+        {
+            Vector2 temp = alpha[i];
+            int randomIndex = Random.Range(i, alpha.Count);
+            alpha[i] = alpha[randomIndex];
+            alpha[randomIndex] = temp;
+        }
     }
 }
