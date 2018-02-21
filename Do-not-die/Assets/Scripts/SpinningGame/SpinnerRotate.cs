cabb59c741d086c1449bf6de1a0e12c887f85513
WolfEngeland <wolfengeland@hotmail.com>
2018-02-21 14:51:48 +0100

    Update Spinning Game

--- Do-not-die/Assets/Scripts/SpinningGame/SpinnerRotate.cs
+++ Do-not-die/Assets/Scripts/SpinningGame/SpinnerRotate.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class SpinnerRotate : MonoBehaviour {
 
-    private Vector2 mouse_position;
-    private Transform target;
-    private Vector2 object_position;
-    private float angle;
-
     private Vector2 mouseScreenPosition;
     private Vector2 direction;
 
@@ -19,14 +14,12 @@ public class SpinnerRotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        direction = (mouseScreenPosition - (Vector2) transform.position).normalized;
+        if (Input.GetMouseButton(0))
+        {
+            mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
 
-        transform.up = direction;
-        /*
-        mouse_position = Input.mousePosition;
-        angle = Mathf.Atan2(mouse_position.y, mouse_position.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-        */	
+            transform.up = direction;
+        }
     }
 }
