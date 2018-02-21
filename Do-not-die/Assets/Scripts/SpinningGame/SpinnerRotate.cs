9c34a346e2fc2634cebf4ae74afbc48cc33b712c
WolfEngeland <wolfengeland@hotmail.com>
2018-02-21 15:50:12 +0100

    Spinning Game Score Counter

--- Do-not-die/Assets/Scripts/SpinningGame/SpinnerRotate.cs
+++ Do-not-die/Assets/Scripts/SpinningGame/SpinnerRotate.cs
@@ -6,6 +6,10 @@ public class SpinnerRotate : MonoBehaviour {
 
     private Vector2 mouseScreenPosition;
     private Vector2 direction;
+    private Vector2 startPoint;
+    private float angleCounter = 0.0f;
+    private float angle = 0.0f;
+    private int score;
 
 	// Use this for initialization
 	void Start () {
@@ -14,12 +18,25 @@ public class SpinnerRotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(startPoint);
+        }
         if (Input.GetMouseButton(0))
         {
             mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
-
             transform.up = direction;
+
+            angle = Vector2.Angle(startPoint, mouseScreenPosition);
+            angleCounter += angle;
+            if (score < Mathf.RoundToInt((angleCounter - (angleCounter % 360)) / 360))
+            {
+                Debug.Log(score);
+            }
+            score = Mathf.RoundToInt((angleCounter - (angleCounter % 360)) / 360);
+            startPoint = mouseScreenPosition;
         }
     }
 }
