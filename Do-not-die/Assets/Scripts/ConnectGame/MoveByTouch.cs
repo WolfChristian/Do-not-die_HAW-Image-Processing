2f5528afa669b8b82c5412eecae61f66caf50200
WolfEngeland <wolfengeland@hotmail.com>
2018-02-24 18:11:28 +0100

    Connect Game Update Base Functions

--- Do-not-die/Assets/Scripts/ConnectGame/MoveByTouch.cs
+++ Do-not-die/Assets/Scripts/ConnectGame/MoveByTouch.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 
 public class MoveByTouch : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject StartPoint;
+    public GameObject EndPoint;
+
+    private bool finished = false;
+    private bool mouseOver = false;
+
+    // Use this for initialization
+    void Start () {
+        transform.position = StartPoint.transform.position;
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (Input.GetMouseButton(0))
+
+    private void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "PointB")
         {
-            var pos = Input.mousePosition;
-            pos.z = 45;
-            pos = Camera.main.ScreenToWorldPoint(pos);
-            transform.position = pos;
+            finished = true;
+            Debug.Log("Hello");
+        }
+    }
+    void FixedUpdate () {
+        if (!finished && mouseOver)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                var pos = Input.mousePosition;
+                pos.z = 45;
+                pos = Camera.main.ScreenToWorldPoint(pos);
+                transform.position = pos;
+            }
+            else
+            {
+                transform.position = StartPoint.transform.position;
+                mouseOver = false;
+            }
+        }
+        else if(finished)
+        {
+            transform.position = EndPoint.transform.position;
         }
     }
 }
