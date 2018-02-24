57a0e213f9a521d1ffc6e398567f34293c7a3bc3
WolfEngeland <wolfengeland@hotmail.com>
2018-02-24 15:20:37 +0100

    Connect Game Start

---
+++ Do-not-die/Assets/Scripts/ConnectGame/MoveByTouch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetMouseButton(0))
+        {
+            var pos = Input.mousePosition;
+            pos.z = 45;
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            transform.position = pos;
+        }
+    }
+}
