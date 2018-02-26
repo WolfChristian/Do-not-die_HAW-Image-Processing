2a796de2dc47859e674f4b2d9776cdc94182a074
WolfEngeland <wolfengeland@hotmail.com>
2018-02-26 18:22:02 +0100

    Touch Fixes Connect Game

--- Do-not-die/Assets/Scripts/ConnectGame/MoveByTouch.cs
+++ Do-not-die/Assets/Scripts/ConnectGame/MoveByTouch.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MoveByTouch : MonoBehaviour {
 
-    public GameObject Controller;
+    private bool moveAllowed = false;
+    private float x, y;
 
-    private bool activ = false;
-    private int nameInInt;
+    public GameObject Controller;
     private bool finished = false;
-    private bool mouseOver = false;
+    private int nameInInt;
     private LineRenderer line;
+
+    //private bool activ = false;
+    //private bool mouseOver = false;
+
     // Use this for initialization
     void Start () {
+        transform.localScale *= 2f;
         nameInInt = System.Convert.ToInt32(this.gameObject.name);
         Controller = GameObject.Find("GameController");
         transform.position = Controller.GetComponent<GameController>().getStartVector(nameInInt);
@@ -25,41 +30,54 @@ public class MoveByTouch : MonoBehaviour {
 
     private void OnMouseOver()
     {
-        mouseOver = true;
+        //mouseOver = true;
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == Controller.GetComponent<GameController>().getName(nameInInt))
         {
             finished = true;
-            Debug.Log("Hello");
+            Controller.GetComponent<GameController>().countConnections();
         }
     }
     void FixedUpdate () {
         line.SetPosition(0, Controller.GetComponent<GameController>().getStartVector(nameInInt));
         line.SetPosition(1, Controller.GetComponent<GameController>().getMovingPointVector(nameInInt));
-
-        if (!finished && mouseOver && Input.GetMouseButtonDown(0))
-        {
-            activ = true;
-        }
-            if (Input.GetMouseButton(0) && activ)
-            {
-                var pos = Input.mousePosition;
-                pos.z = 45;
-                pos = Camera.main.ScreenToWorldPoint(pos);
-                transform.position = pos;
-            }
-            else
-            {
-                transform.position = Controller.GetComponent<GameController>().getStartVector(nameInInt);
-                mouseOver = false;
-                activ = false;
-            }
-        
         if(finished)
         {
             transform.position = Controller.GetComponent<GameController>().getEndVector(nameInInt);
+        }else
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+                switch (touch.phase)
+                {
+                    //If the object is touched you are able to move it
+                    case TouchPhase.Began:
+
+                        if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                        {
+                            moveAllowed = true;
+                            transform.position = touchPos;
+                        }
+                        break;
+                    //While movemen is allowed set the position of the object to new position of the touch input
+                    case TouchPhase.Moved:
+                        if (moveAllowed)
+                        {
+                            transform.position = touchPos;
+                        }
+                        break;
+                    //If the touch input ends objects is not allowed to move again
+                    case TouchPhase.Ended:
+                        moveAllowed = false;
+                        transform.position = Controller.GetComponent<GameController>().getStartVector(nameInInt);
+                        break;
+                }
+            }
         }
     }
 }
